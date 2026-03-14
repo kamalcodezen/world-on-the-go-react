a3a3ea6b5fa1409f249b1939f628d5d6b54f0185
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-15 03:56:23 +0530

    css style add

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -1,19 +1,23 @@
 import React, { use } from "react";
 import Country from "../Country/Country";
+import "./Countries.css"
+
 
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
-//   console.log(countries);
+  //   console.log(countries);
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>
         Total Countries {countries.length}
       </h1>
 
-      {countries.map((country) => (
-        <Country country={country}></Country>
-      ))}
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/component/country/country.jsx
+++ src/component/country/country.jsx
@@ -1,25 +1,28 @@
 import React from "react";
-
+import "./Country.css";
 const Country = ({ country }) => {
-  console.log(country);
+//   console.log(country);
+
+const handleVisited = ()=>{
+    console.log("buton clicked")
+}
 
   return (
-    <div style={{ textAlign: "center", display:"flex", marginBottom:"30px"}}>
 
-      <div style={{width:"600px"}}>
 
-        <h3>Country Name : {country.name.official}</h3>
-
-        <h4>Country Population : {country.population.population}</h4>
-        
-        <h4>Country Region : {country.region.region}</h4>
-
-      </div>
-
+    <div className="country">
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
 
-    </div>
+      <div>
+        <h3>Country Name : {country.name.common}</h3>
 
+        <h4>Country Population : {country.population.population > 3000000 ? "Big Country": "Small Country"}</h4>
+
+        <h4>Region : {country.region.region}</h4>
+
+        <button onClick={()=>handleVisited(country.ccn3.name)} className="countryBtn">Visited</button>
+      </div>
+    </div>
   );
 };
 
