31747fbcdf3628d6cbe8d5ce49f1329730063ac2
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-15 12:24:52 +0530

    visited country usestate update new array

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -5,23 +5,35 @@ import "./Countries.css";
 const Countries = ({ countriesPromise }) => {
   const [visitedCountry, setVisitedCountry] = useState([]);
 
+  const handleVisitedCountry = (country) => {
+    const updateVisitedCountry = [...visitedCountry, country];
+    setVisitedCountry(updateVisitedCountry);
+  };
+
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
   //   console.log(countries);
-
-  const handleVisitedCountry = (country) => {
-    console.log("visited button clicked",country);
-  };
 
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>
         Total Countries {countries.length}
       </h1>
+      <h2>Visited Country : {visitedCountry.length}</h2>
+      <ol>
+       {
+        visitedCountry.map(visitCountry => <li>{visitCountry.name.common}</li>)
+
+       }
+      </ol>
 
       <div className="countries">
         {countries.map((country) => (
-          <Country handleVisitedCountry={handleVisitedCountry} key={country.cca3.cca3} country={country}></Country>
+          <Country
+            handleVisitedCountry={handleVisitedCountry}
+            key={country.cca3.cca3}
+            country={country}
+          ></Country>
         ))}
       </div>
     </div>
